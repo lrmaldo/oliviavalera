f4e2734fe398a4a4a8a80679ac9905c78996c8dd
leo maldonado <lrmaldo@gmail.com>
2025-04-14 16:38:37 -0600

    fix: Restaurar enlaces de scripts y estilos en la vista del carrusel

--- resources/views/hotspot/requests/carrusel.blade.php
+++ resources/views/hotspot/requests/carrusel.blade.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Campaña Tierra Blanca - WiFi Gratuito</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <script src="/js/3.4.16.js"></script>
+    <link rel="stylesheet" href="/fontawesome/css/all.min.css">
     <style>
         .bg-pri-green {
             background-color: #006847;
